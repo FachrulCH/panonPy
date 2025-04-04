3f269931d7a205cebb0f0f35458c356020da9c5e
fachrulch <fachrul.fch@gmail.com>
2025-04-04 18:21:53 +0700

    improve the param and report html

--- tests/test_visual_test.py
+++ tests/test_visual_test.py
@@ -2,15 +2,11 @@ from panonpy.panonpy import PanonPy
 
 
 def test_simple_test():
-    comparer = PanonPy()
-    result = comparer.compare_images(
-        '/Users/fachrulch/py/panonpy/tests/visualtest/keduax.png',
-        '/Users/fachrulch/py/panonpy/tests/visualtest/baseline/pertamax.png',
-        diff_path='/Users/fachrulch/py/panonpy/tests/visualtest/diff.png',
+    tester = PanonPy(output_dir='results')
+
+    tester.compare_images(
+        actual_image_path='/Users/fachrulch/py/panonpy/tests/resources/actual.png',
+        baseline_image_path='/Users/fachrulch/py/panonpy/tests/resources/baseline.png',
+        test_name='pertamax_test',
         threshold=0.01
     )
-
-    if result:
-        print("Images are similar within the threshold.")
-    else:
-        print("Images are different beyond the threshold.")
